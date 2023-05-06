2eb713118dbe40573ad774c98e9ddebae1db5dc1
Diegom03 <diegomartinm03@gmail.com>
2023-05-06 17:22:07 +0200

    Navigation
    dependencias necesarias para crear screens asi como navegar entre ellas

--- cigood_app/App.js
+++ cigood_app/App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Text>Pantalla principal</Text>
+      <Button
+        title="Ir a pantalla de detalles"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View>
+      <Text>Pantalla de detalles</Text>
+    </View>
+  );
+}
+
+// CADA VEZ QUE SE INSTALE ALGO ==> npm start --reset-cache
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
