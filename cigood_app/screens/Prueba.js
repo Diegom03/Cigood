fc22ab861d2a1aec2b4d2ea919b8a809fe0bb135
Diegom03 <diegomartinm03@gmail.com>
2023-05-16 21:30:12 +0200

    IA

--- cigood_app/screens/Prueba.js
+++ cigood_app/screens/Prueba.js
@@ -10,15 +10,24 @@ const MyScreen = () => {
         const tabla = "recetas";
         const id = "644ab1bebb62c636801a0d94";
 
+        //ETSO OBTIENE TODAS LAS RECETAS
         // fetch(`http://192.168.1.139:3000/api/data/${tabla}/${id}`)
         //     .then(response => response.json())
         //     .then(data => setRecipes(data))
         //     .catch(error => console.error(error));
 
+        //ESTO VACIA LA DESPENSA
         fetch(`http://192.168.1.139:3000/api/despensa/${id}`)
             .then(response => response.json())
             .then(data => setRecipes(data))
             .catch(error => console.error(error));
+
+        //ESTO PILLA LA IA
+
+        // fetch('https://world.openfoodfacts.org/api/v0/product/8412833203800.json')
+        //     .then(response => response.json())
+        //     .then(data => console.log(data))
+        //     .catch(error => console.error(error));
     };
 
     const getCamara = () => {
