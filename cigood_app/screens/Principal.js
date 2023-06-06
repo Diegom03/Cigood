256c052844944e9561e3a39b509ac766414192f8
byniched <123072356+byniched@users.noreply.github.com>
2023-06-07 00:53:13 +0200

    Principal estilos

--- cigood_app/screens/Principal.js
+++ cigood_app/screens/Principal.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, FlatList, Modal, TouchableWithoutFeedback, ActivityIndicator } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { getIngredientes, recetasDespensa } from '../Onload';
+import { getIngredientes, recetasDespensa, getRecetas, getNameIngredientes } from '../Onload';
 import Carousel from 'react-native-snap-carousel';
 
 const Principal = () => {
@@ -11,6 +11,10 @@ const Principal = () => {
     const [modalVisible, setModalVisible] = useState(false);
     const [selectedRecipe, setSelectedRecipe] = useState('');
     const [isLoading, setIsLoading] = useState(true);
+    const [recetaDelDia, setRecetaDelDia] = useState(null);
+    const [allRecipes, serRecipes] = useState([]);
+    const [ingredientesModal, setIngredientes] = useState([]);
+    const [showRecetaDelDia, setShowRecetaDelDia] = useState(false);
 
     useEffect(() => {
         const obtenerRecetas = async () => {
@@ -28,6 +32,38 @@ const Principal = () => {
 
         obtenerRecetas();
     }, []);
+
+    useEffect(() => {
+        const obtenerAllRecetas = async () => {
+            try {
+                if (recetas.length > 0) {
+                    const recetasBD = await getRecetas();
+                    const randomIndex = Math.floor(Math.random() * 27);
+                    const nuevaRecetaDelDia = recetasBD[randomIndex];
+                    setRecetaDelDia(nuevaRecetaDelDia);
+                }
+            } catch (error) {
+                console.error('Error al obtener los productos:', error);
+            }
+        };
+
+        obtenerAllRecetas();
+    }, [recetas]);
+
+    const obtenerIngredientes = async (datosReceta) => {
+        try {
+            if (recetaDelDia && recetaDelDia._ingredientes) {
+                const data = await getNameIngredientes(datosReceta._ingredientes);
+                console.log("hola" + JSON.stringify(data));
+                setIngredientes(data);
+            }
+        } catch (error) {
+            console.error('Error al obtener los ingredientes:', error);
+        }
+    };
+
+
+
 
     useEffect(() => {
         const obtenerRecetasDiarias = async () => {
@@ -60,6 +96,7 @@ const Principal = () => {
     };
     const abrirModalReceta = (id) => {
         const datosReceta = id;
+        obtenerIngredientes(datosReceta);
         setSelectedRecipe(datosReceta);
         setModalVisible(true);
     };
@@ -84,10 +121,47 @@ const Principal = () => {
                 <View style={styles.modalTextContainer}>
                     <Text style={styles.modalRecipeTitle}>{selectedRecipe._descripcion}</Text>
                     <Text style={styles.modalRecipeTime}>Tiempo de preparación: {selectedRecipe._tiempo}</Text>
+                    <View style={styles.ingredientItemsContainer}>
+                        <Text style={styles.modalIngredientes}>Ingredientes:</Text>
+                        {ingredientesModal.map((ingrediente, index) => (
+                            <Text key={index} style={styles.ingredientItem}>
+                                - {ingrediente._nombre}
+                            </Text>
+                        ))}
+                    </View>
                 </View>
             </View>
         );
     };
+    const renderRecetaDelDia = () => {
+        if (!showRecetaDelDia) {
+          return (
+            <TouchableOpacity
+              onPress={() => setShowRecetaDelDia(true)}
+              style={styles.recetaDelDiaContainer}
+            >
+              <View style={styles.recetaDelDiaOverlay}>
+                <Text style={styles.recetaDelDiaOverlayText}>Pulsa para revelar</Text>
+              </View>
+              <Image source={{ uri: recetaDelDia._img }} style={styles.recetaDelDiaImage} />
+              <View style={styles.recetaDelDiaTextContainer}>
+                <Text style={styles.recetaDelDiaTitle}>{recetaDelDia._descripcion}</Text>
+                <Text style={styles.recetaDelDiaTitle}>{recetaDelDia._tiempo}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        }
+    
+        return (
+          <TouchableOpacity onPress={() => abrirReceta(recetaDelDia)} onLongPress={() => abrirModalReceta(recetaDelDia)} style={styles.recetaDelDiaContainer}>
+            <Image source={{ uri: recetaDelDia._img }} style={styles.recetaDelDiaImage} />
+            <View style={styles.recetaDelDiaTextContainer}>
+              <Text style={styles.recetaDelDiaTitle}>{recetaDelDia._descripcion}</Text>
+              <Text style={styles.recetaDelDiaTime}>{recetaDelDia._tiempo}</Text>
+            </View>
+          </TouchableOpacity>
+        );
+      };
 
     return (
         <View style={styles.container}>
@@ -110,6 +184,12 @@ const Principal = () => {
                     itemWidth={200}
                     layout="default"
                 />)}
+            <Text style={styles.title2}>Receta del dia</Text>
+            {recetaDelDia ? (
+                renderRecetaDelDia()
+            ) : (
+                <ActivityIndicator size="large" color="#FF5555" />
+            )}
             <Modal visible={modalVisible} animationType="fade" transparent>
                 <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
                     <View style={styles.modalContainer}>
@@ -117,15 +197,13 @@ const Principal = () => {
                     </View>
                 </TouchableWithoutFeedback>
             </Modal>
-            <Text style={styles.title2}>Receta del dia</Text>
+
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'flex-start',
         alignItems: 'center',
         backgroundColor: '#F2EFE9',
     },
@@ -153,35 +231,35 @@ const styles = StyleSheet.create({
         fontSize: 30,
         fontWeight: 'bold',
         textAlign: 'center',
-        color: '#E12626',
-        marginTop: 30,
-        marginBottom: 30,
+        color: '#FF7F50',
+        marginTop: 20,
+        marginBottom: 10,
     },
-
-    // FLATLIST
-    recipeContainer: {
-        display: 'flex',
-        height:80
-    },
+    // CARRUSEL
     recipeItem: {
         borderRadius: 8,
         paddingTop: 10,
         margin: 0,
-        backgroundColor: '#FF8A6B',
+        backgroundColor: '#FF6347',
         alignItems: 'center',
+    },
+    recipeContainer: {
+        display: 'flex',
+        height: 60,
+        justifyContent: 'center',
     },
     recipeImage: {
         width: 140, //150
         height: 140, //150
         borderRadius: 5,
-        alignSelf:'center',
+        alignSelf: 'center',
     },
     recipeTitle: {
         fontSize: 17,
         fontWeight: '600',
         textAlign: 'center',
-        paddingVertical: 20,
         color: 'black',
+        maxWidth: 150,
     },
     //MODAL
     modalContainer: {
@@ -217,6 +295,9 @@ const styles = StyleSheet.create({
         fontSize: 16,
         marginBottom: 10,
     },
+    modalIngredientes: {
+        fontSize: 16,
+    },
     loadingContainer: {
         flex: 1,
         justifyContent: 'center',
@@ -228,6 +309,76 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#FF5555'
     },
+
+    //RECETA DEL DIA
+    recetaDelDiaContainer: {
+        borderRadius: 8,
+        margin: 0,
+        width: 300,
+        height: 200,
+        backgroundColor: '#008C6C',
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        marginBottom: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        flexDirection: 'row',
+    },
+    recetaDelDiaOverlay: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0, 0, 0, 0.9)',
+        justifyContent: 'center',
+        alignItems: 'center',
+        zIndex:2,
+        // Otros estilos para la superposición de la receta del día
+      },
+      recetaDelDiaOverlayText: {
+        color: '#FFFFFF',
+        fontSize: 20,
+        fontWeight: 'bold',
+        // Otros estilos para el texto de superposición de la receta del día
+      },
+    recetaDelDiaImage: {
+        width: 140,
+        height: 140,
+        borderRadius: 5,
+        alignSelf: 'center',
+    },
+    recetaDelDiaTitle: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: 'white',
+        maxWidth: 180,
+        paddingVertical: 10,
+        //fontWeight: '500',
+    },
+    ingredientItem: {
+        fontSize: 16,
+        marginLeft: 8,
+
+    },
+    recetaDelDiaTextContainer: {
+        flex: 1,
+        marginLeft: 10,
+        justifyContent: 'center',
+    },
+    ingredientContainer: {
+        marginTop: 10,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start', // Alinear los ingredientes al inicio del contenedor
+    },
+
 });
 
 export default Principal;
